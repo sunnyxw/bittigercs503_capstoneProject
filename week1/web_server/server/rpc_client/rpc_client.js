3d43bd645b01c36f470394011df9cca48b78f679
Xiaowei Yang <christianaweather@gmail.com>
2017-07-16 11:20:27 -0400

    update webserver config

--- week1/web_server/server/rpc_client/rpc_client.js
+++ week1/web_server/server/rpc_client/rpc_client.js
@@ -1,30 +1,38 @@
 var jayson = require('jayson');
+const config = require('../config/config.json');
 
-var ConfigClient = jayson.client.http({
-    port:4000,
-    hostname:'localhost'
-});
+// var ConfigClient = jayson.client.http({
+//     port:4000,
+//     hostname:'localhost'
+// });
 
-function backendServerConfig(callback){
-    ConfigClient.request('backendServerConfig',[],function(err, error, response){
-        if (err) throw err;
-        // console.log(response);
-        // console.log(response.SERVER_HOST)
-        return callback(response);
-    });
-}
+// function backendServerConfig(callback){
+//     ConfigClient.request('backendServerConfig',[],function(err, error, response){
+//         if (err) throw err;
+//         // console.log(response);
+//         // console.log(response.SERVER_HOST)
+//         return callback(response);
+//     });
+// }
 
-console.log(backendServerConfig(function(response) {
-   res.json(response);
-}));
+// console.log(backendServerConfig(function(response) {
+//    res.json(response);
+// }));
+
+// var SERVER_PORT;
+// var SERVER_HOST;
+
+// ConfigClient.request('backendServerConfig', [], function(err, error, response){
+//         if (err) throw err;
+//         SERVER_PORT = response.SERVER_PORT;
+//         SERVER_HOST = response.SERVER_HOST;
+//         console.log(SERVER_PORT)
+// })
 
 var client = jayson.client.http({
-    // port: SERVER_PORT,
-    // hostname: SERVER_HOST
-    port:1234,
-    hostname: 'localhost'
-});
-
+    port: config.SERVER_PORT,
+    hostname: config.SERVER_HOST
+})
 
 //Test RPC method
 function add(a, b, callback){
